83b61f1072c4633817a7b9f59c8b3927fe3b5bfe
gwangWook218 <akw2080@naver.com>
2024-07-24 17:08:21 +0900

    레이아웃 예제

---
+++ ex0723/js/menu1.js
@@ -0,0 +1,8 @@
+$("header nav>ul").hover(function () {
+        // over
+        $(this).find('.submenu').stop().slideDown(1000)
+    }, function () {
+        // out
+        $('.submenu').stop().slideUp(500)
+    }
+);

ee5ef7632a2b35d8e4036c56b99eb8d8febc3ea9
gwangWook218 <akw2080@naver.com>
2024-07-25 17:02:48 +0900

    웹디자인기능사 예제

--- ex0723/js/menu1.js
+++ ex0723/js/menu1.js
@@ -1,8 +1,10 @@
-$("header nav>ul").hover(function () {
-        // over
-        $(this).find('.submenu').stop().slideDown(1000)
-    }, function () {
-        // out
-        $('.submenu').stop().slideUp(500)
-    }
-);
+$(document).ready(function () {
+    $('.mainmenu').hover(function () {
+            // over
+            $('.submenu').stop().slideDown(500);
+        }, function () {
+            // out
+            $('.submenu').stop().slideUp(500);
+        }
+    );
+});

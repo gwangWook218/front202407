5390cc04284c06639147902aee1de48c57e34def
gwangWook218 <akw2080@naver.com>
2024-07-26 17:00:23 +0900

    jQuery

---
+++ ex0726/js/testjQuery.js
@@ -0,0 +1,32 @@
+$("button").click(function () { 
+    $("p").hide();
+});
+
+$("#delDiv").click(function () { 
+    $("#test").hide();
+});
+
+$("#delDiv2").click(function () { 
+    $(".del").hide();
+});
+
+$("#dblclick").dblclick(function (e) { 
+    $(this).hide();
+});
+
+$("#enter").mouseenter(function () { 
+    $(this).text("오버했습니다.");
+});
+
+$("#enter").mouseleave(function () { 
+   $(this).text("벗어났습니다."); 
+});
+
+$("#hover").hover(function () {
+        // over
+        $(this).text("마우스 OVER오버");
+    }, function () {
+        // out
+        $(this).text("마우스 오버OUT ~ 빠이~");
+    }
+);
